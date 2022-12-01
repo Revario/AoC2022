7ab8535d23ab46139f8cb0e02d2211d85e43aae5
Christopher Andersson <reekk@hotmail.com>
2022-12-01 13:17:59 +0100

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,22 @@
+﻿var reader = File.ReadLines("input.txt");
+
+
+List<int> caloriesPerElf = new ();
+
+int calories = 0;
+
+foreach(string line in reader)
+{
+    if (string.IsNullOrEmpty(line))
+    {
+        caloriesPerElf.Add(calories);
+        calories = 0;
+        continue;
+    }
+    calories += int.Parse(line);
+}
+
+caloriesPerElf.Sort();
+
+Console.WriteLine($"Part1: {caloriesPerElf.Last()}");
+Console.WriteLine($"Part2: {caloriesPerElf.TakeLast(3).Sum()}");
