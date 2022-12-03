bd5ce47b6aa9be5dd88fc79355afb1f115325e4d
Christopher Andersson <reekk@hotmail.com>
2022-12-03 09:29:53 +0100

    Day3

---
+++ Day3/Program.cs
@@ -0,0 +1,30 @@
+﻿var lines = File.ReadAllLines(new Utils.Input("3").GetInputFilePath());
+
+
+var s = lines.Sum(l =>
+    GetPriority(
+        l[..(l.Length / 2)]
+        .Intersect(l[(l.Length / 2)..])
+        .Single()
+        )
+);
+
+Console.WriteLine(s);
+
+var p = lines
+    .Chunk(3)
+    .Sum(l =>
+        GetPriority(
+            l[0]
+            .Intersect(l[1])
+            .Intersect(l[2])
+            .Single())
+);
+
+
+Console.WriteLine(p);
+
+int GetPriority(char c)
+{
+    return char.IsUpper(c) ? (int)c - 38 : (int)c - 96;
+}
