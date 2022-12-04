f254e9b074dc9c3587b6a80a57695c5ad4b25e10
Christopher Andersson <reekk@hotmail.com>
2022-12-04 09:30:53 +0100

    Day4

---
+++ Day4/Program.cs
@@ -0,0 +1,40 @@
+﻿var inputs = File.ReadLines(new Utils.Input("4").GetInputFilePath());
+
+
+
+
+var s = inputs.Select(i =>
+{
+    var a = i.Split(',').SelectMany(ss => ss.Split('-')).ToArray();
+    return new
+    {
+        firstStart = int.Parse(a[0]),
+        firstEnd = int.Parse(a[1]),
+        secondStart = int.Parse(a[2]),
+        secondEnd = int.Parse(a[3])
+    };
+}).ToArray();
+
+
+
+
+
+
+Console.WriteLine(s.Count(a =>
+    (a.firstStart <= a.secondStart
+    && a.firstEnd >= a.secondEnd)
+    ||
+    (a.secondStart <= a.firstStart
+    && a.secondEnd >= a.firstEnd)
+));
+
+
+
+Console.WriteLine(s.Count(a =>
+    (a.firstStart <= a.secondStart
+    && a.firstEnd >= a.secondStart)
+    ||
+    (a.secondStart <= a.firstStart
+    && a.secondEnd >= a.firstStart)
+));
+
