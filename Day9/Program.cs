d3b97fdd4582f36109a122fe2d3b4a525143aa53
Revario <christopher.andersson@silicnorth.se>
2022-12-09 17:14:31 +0100

    Day 9 Continued

--- Day9/Program.cs
+++ Day9/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Xml.Serialization;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Serialization;
 
 var input = File.ReadLines(Utils.Input.GetInputFilePath(9));
 
@@ -16,80 +18,97 @@ inp.Split(" ") switch
 ).ToList();
 
 
-var r = new Rope();
+//var part1 = new Rope(1);
+var part2 = new Rope(9);
 
 foreach(var i in ins)
 {
-    r.MoveHead(i);
+    //part1.MoveHead(i);
+    part2.MoveHead(i);
 }
 
-Console.WriteLine(r.NumberOfKnownTailpositions());
+//Console.WriteLine(part1.NumberOfKnownTailPositions());
+Console.WriteLine(part2.NumberOfKnownTailPositions());
 
 
 
 
 class Rope
 {
-    HashSet<string> tailPositions= new();
+    HashSet<string> tailPositions = new();
 
-    Head head = new();
-    Tail tail = new();
+    Head _head = new();
+    Knot[] _knots;
 
-    public int NumberOfKnownTailpositions()
+    public Rope(int sizeOfTail)
+    {
+        _knots = new Knot[sizeOfTail];
+        for (int i = 0; i < _knots.Length; i++)
+        {
+            _knots[i] = new();
+        }
+    }
+    public int NumberOfKnownTailPositions()
     {
         return tailPositions.Count;
     }
+
     public void MoveHead(Instruction ins)
     {
         for (int i = 0; i < ins.numberOfSteps; i++)
         {
             _=ins.dir switch
             {
-                Direction.Left => head.x--,
-                Direction.Right => head.x++,
-                Direction.Up => head.y++,
-                Direction.Down => head.y--,
+                Direction.Left => _head.x--,
+                Direction.Right => _head.x++,
+                Direction.Up => _head.y++,
+                Direction.Down => _head.y--,
             };
-            MoveTail();
+            MoveTail(_head, _knots);
         }
     }
 
-    private void MoveTail()
+    private void MoveTail(Knot prev, Knot[] knots)
     {
-        if(Math.Abs(head.x - tail.x) == 2 )
+        var k = knots.FirstOrDefault();
+
+        if(k is null) { return; }
+
+        if(Math.Abs(prev.x - k.x) == 2 )
         {
-            tail.y = head.y;
-            if(head.x > tail.x)
+            k.y = prev.y;
+            if(prev.x > k.x)
             {
-                tail.x++;
+                k.x++;
             } else
             {
-                tail.x--;
+                k.x--;
             }
-        } else if(Math.Abs(head.y - tail.y) == 2 )
+        } else if(Math.Abs(prev.y - k.y) == 2 )
         {
-            tail.x = head.x;
-            if(head.y > tail.y)
+            k.x = prev.x;
+            if(prev.y > k.y)
             {
-                tail.y++;
+                k.y++;
             } else
             {
-                tail.y--;
+                k.y--;
             }
         }
-        string stringPos = $"{tail.x},{tail.y}";
-        if (!tailPositions.Contains(stringPos))
+        string stringPos = $"{k.x},{k.y}";
+        if (knots.Length == 1 && !tailPositions.Contains(stringPos))
         {
             tailPositions.Add(stringPos);
         }
+        MoveTail(k, knots[1..]);
     }
 
-    class Head
+    class Head : Knot
     {
-        public int x;
-        public int y;
+        //public int x;
+        //public int y;
     }
-    class Tail
+    class Knot
     {
         public int x;
         public int y;
