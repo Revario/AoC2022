e410ead84993063c78e71ae97ef9d6c9d3c08a65
Revario <christopher.andersson@silicnorth.se>
2022-12-11 06:43:48 +0100

    Start of day 11

---
+++ Day11/Program.cs
@@ -0,0 +1,16 @@
+﻿var input = File.ReadAllText(Utils.Input.GetInputFilePath(11));
+
+
+
+var monkeyInput = input.Split("\n\n");
+
+Console.WriteLine(monkeyInput[0]);
+
+
+
+class Monkey
+{
+    public List<int> Items { get; set; }
+    public Func<int, int> Operation { get; set; }
+    public Func<int, int> Test { get; set; }
+}
