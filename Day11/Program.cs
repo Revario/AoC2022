bca815d6e7281ec82260700fdc3943f3382cbe36
Christopher Andersson <reekk@hotmail.com>
2022-12-11 08:49:12 +0100

    Start of day11

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -5,3 +5,25 @@
 var monkeyInput = input.Split("\n\n");
 
 Console.WriteLine(monkeyInput[0]);
+
+
+
+
+
+
+
+class Monkey
+{
+    public List<int> Items { get; set; } = default!;
+    private Func<int, int> Operation = default!;
+    private Action<int> Action = default!;
+
+
+    public Monkey(List<int> items, Func<int, int> operation, Action<int> action)
+    {
+        Items = items;
+        Operation = operation;
+        Action = action;
+    }
+
+}
