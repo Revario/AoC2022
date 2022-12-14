a0489eedb35510e15e6c8b56dada07d9ab800247
Revario <christopher.andersson@silicnorth.se>
2022-12-14 14:36:46 +0100

    Fixed newline change due to github

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -2,7 +2,7 @@
 
 //input = "Monkey 0:\r\n  Starting items: 79, 98\r\n  Operation: new = old * 19\r\n  Test: divisible by 23\r\n    If true: throw to monkey 2\r\n    If false: throw to monkey 3\r\n\r\nMonkey 1:\r\n  Starting items: 54, 65, 75, 74\r\n  Operation: new = old + 6\r\n  Test: divisible by 19\r\n    If true: throw to monkey 2\r\n    If false: throw to monkey 0\r\n\r\nMonkey 2:\r\n  Starting items: 79, 60, 97\r\n  Operation: new = old * old\r\n  Test: divisible by 13\r\n    If true: throw to monkey 1\r\n    If false: throw to monkey 3\r\n\r\nMonkey 3:\r\n  Starting items: 74\r\n  Operation: new = old + 3\r\n  Test: divisible by 17\r\n    If true: throw to monkey 0\r\n    If false: throw to monkey 1";
 
-var monkeyInput = input.Split(Environment.NewLine + Environment.NewLine).Select(mI => Monkey.Parse(mI)).ToList();
+var monkeyInput = input.Split("\n\n").Select(mI => Monkey.Parse(mI)).ToList();
 
 for (int i = 0; i < 20; i++)
 {
@@ -54,7 +54,7 @@ class Monkey
         var operation = lines[2].Split("=")[1].Trim().Split(" ") switch
         {
             ["old", "*", "old"] => (int old) => old * old,
-            ["old", "*", var numS] when int.TryParse(numS, out var num) => (Func<int,int>)((int old) => old * num),
+            ["old", "*", var numS] when int.TryParse(numS, out var num) => (Func<int, int>)((int old) => old * num),
             ["old", "+", var numS] when int.TryParse(numS, out var num) => (Func<int, int>)((int old) => old + num)
         };
 
