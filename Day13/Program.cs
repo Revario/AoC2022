cfee4decb94ea24d77fdb7c394e6265135b91534
Christopher Andersson <reekk@hotmail.com>
2022-12-15 18:16:12 +0100

    Another failed attempt at day 13

--- Day13/Program.cs
+++ Day13/Program.cs
@@ -1,100 +1,114 @@
-﻿var input = File.ReadAllText(Utils.Input.GetInputFilePath(13)).Split("\n\n").Select(s => s.Split("\n")).Select(i => (i[0], i[1])).ToList();
+﻿using Microsoft.VisualBasic;
+using System.Security.AccessControl;
+
+var input = File.ReadAllText(Utils.Input.GetInputFilePath(13)).Split("\n\n").Select(s => s.Split("\n")).Select(i => (i[0], i[1])).ToList();
 
 input = "[1,1,3,1,1]\n[1,1,5,1,1]\n\n[[1],[2,3,4]]\n[[1],4]\n\n[9]\n[[8,7,6]]\n\n[[4,4],4,4]\n[[4,4],4,4,4]\n\n[7,7,7,7]\n[7,7,7]\n\n[]\n[3]\n\n[[[]]]\n[[]]\n\n[1,[2,[3,[4,[5,6,7]]]],8,9]\n[1,[2,[3,[4,[5,6,0]]]],8,9]".Split("\n\n").Select(s => s.Split("\n")).Select(i => (i[0], i[1])).ToList();
 
-Console.WriteLine(input);
-
-List<int> correct = new();
 
 
-for (int i = 0; i < input.Count; i++)
+int correct = 0;
+foreach(var p in input)
 {
-    var (left, right) = input[i];
+    if(UnpackAndCompare(p.Item1, p.Item2)) correct++;
+}
 
-    left = ReplaceChars(left);
-    right = ReplaceChars(right);
-
-    //var leftParts = left.Split(",");
-    //var rightParts = right.Split(",");
-
-
-    for (int l = 0; l < left.Length; l++)
+bool UnpackAndCompare(string left, string right)
+{
+    if(string.IsNullOrEmpty(left) && !string.IsNullOrEmpty(right))
     {
-        var lchar = left[l];
+        return false;
+    }
+    else if (string.IsNullOrEmpty(right))
+    {
+        return true;
+    }
 
-        if(l >= right.Length)
-        {
-            break;
-        }
-        var rchar = right[l];
+    (left, right) = Unpack(left, right);
 
-        if(lchar == '#')
-        {
-            correct.Add(i + 1);
-            break;
-        }
-        
-        if (lchar == '#')
-        {
-            break;
-        }
 
-        if ((int)lchar > (int)rchar)
+    var l = left.Split(",");
+    var r = right.Split(",");
+
+
+    for (int i = 0; i < l.Length; i++)
+    {
+        if (l[i].StartsWith('[') && r[i].StartsWith('['))
         {
-            break;
+            return UnpackAndCompare(l[i], r[i]);
         }
-        if ((int)lchar < (int)rchar)
+    
+        if (l[i].StartsWith('['))
         {
-            correct.Add(i + 1);
-            break;
+            return UnpackAndCompare(l[i], $"[{r[i]}]");
+        } else if (r[i].StartsWith('['))
+        {
+            return UnpackAndCompare($"[{l[i]}]", r[i]);
         }
 
-        if(l == left.Length - 1)
+        int lV = ConvertToInt(l[i]);
+        int rV = ConvertToInt(r[i]);
+
+        if (lV < rV)
         {
-            correct.Add(i + 1);
+            return true;
+        } else if (lV > rV)
+        {
+            return false;
         }
     }
+    //Compare(left, right);
+
+    throw new ArgumentException();
 }
 
-Console.WriteLine(correct.Sum());
-//Console.WriteLine(correct.Aggregate((prev, next) => prev * next));
 
-string ReplaceChars(string input)
+int ConvertToInt(string inp)
 {
-    return input.Replace("[]", "#").Replace("[", string.Empty).Replace("]", string.Empty).Replace(",", string.Empty);
+    if(inp.Length != 1)
+    {
+        throw new ArgumentException();
+    }
+
+    return (int)inp[0];
 }
 
 
+//var (left, right) = input.First();
 
-//for (int signalNum = 0; signalNum < input.Count; signalNum++)
+
+//for (int i = 0; i < left.Length; i++)
 //{
-//	var (left, right) = input[signalNum];
-
-//    var rightCounter = 0;
-//    for (int i = 0; i < left.Length; i++, rightCounter++)
-//    {
-//        if (i + 1 < left.Length && left[i] == '[' && left[i + 1] == ']')
-//        {
-//            correct.Add(i);
-//            break;
-//        }
-
-//        if (left[i] == '[')
-
-//    }
-//}
-
-
-
-
-//class SignalPair
-//{
-
 
 //}
 
 
-//class Signal
+//if(Unpack(left, out var unp))
 //{
-//    public string signal { get; set; }
+//    Console.WriteLine(unp);
 //}
+(string, string) Unpack(string left, string right)
+{
+    //output = input;
+
+    //if (string.IsNullOrEmpty(input))
+    //{
+    //    return false;
+    //}
+
+    //if (input.StartsWith('['))
+    //{
+
+    return (left[1..^1], right[1..^1]);
+
+    //    return true;    
+    //}
+
+    //if(int.TryParse(input[0].ToString(), out var _))
+    //{
+    //    return true;
+    //}
+
+    //throw new ArgumentException("Could not unpack, unexpected input");
+}
+
