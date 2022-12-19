963bdb0083f02b529c8bab0dfe45d73ff661f0e2
Christopher Andersson <reekk@hotmail.com>
2022-12-19 19:14:13 +0100

    Yet another attempt on day 13

--- Day13/Program.cs
+++ Day13/Program.cs
@@ -1,157 +1,145 @@
-﻿using Microsoft.VisualBasic;
-using System.Security.AccessControl;
+﻿
 
-var input = File.ReadAllText(Utils.Input.GetInputFilePath(13)).Split("\n\n").Select(s => s.Split("\n")).Select(i => (i[0], i[1])).ToList();
-
-input = "[1,1,3,1,1]\n[1,1,5,1,1]\n\n[[1],[2,3,4]]\n[[1],4]\n\n[9]\n[[8,7,6]]\n\n[[4,4],4,4]\n[[4,4],4,4,4]\n\n[7,7,7,7]\n[7,7,7]\n\n[]\n[3]\n\n[[[]]]\n[[]]\n\n[1,[2,[3,[4,[5,6,7]]]],8,9]\n[1,[2,[3,[4,[5,6,0]]]],8,9]".Split("\n\n").Select(s => s.Split("\n")).Select(i => (i[0], i[1])).ToList();
-
-
-
-int correct = 0;
-
-for (int s = 0; s < input.Count; s++)
+public class Program
 {
-	var l = input[s].Item1;
-	var r = input[s].Item2;
-
-	while (true) {
-		if (string.IsNullOrEmpty(l))
-		{
-			correct += s + 1;
-			break;
-		} else if (string.IsNullOrEmpty(r))
-		{
-			break;
-		}
-
-
-
-		//if (l[li] == '[')
-		//{
-		//	if (r[ri] == '[')
-		//	{
-		//		continue;
-		//	} else
-		//	{
-		//		r = r.Insert(ri + 1, "]");
-		//              li++;
-		//	}
-		//} else if (r[ri] == '[')
-		//{
-		//	l = l.Insert(li + 1, "]");
-		//	ri++;
-		//}
-
-		//if (l[li] == ']') 
-		//{
-		//	if (r[ri] == ']')
-		//	{
-		//		continue;
-		//	} else
-		//	{
-		//              correct += s + 1;
-		//		break;
-		//	}
-		//} else if (r[ri] == ']')
-		//{
-		//	break;
-		//}
-
-
-
-		//TODO hantera slutbracket när enskild siffra wrappas
-
-		(l, r) = Unpack(l, r);
-
-		if (l[0] != ']' && r[0] == ']')
-		{
-			break;
-		}
-
-		if (l[0] < r[0] || l[0] == ']')
-		{
-			correct += s + 1;
-			break;
-		}
-		
-		if (l[0] > r[0])
-		{
-			break;
-		}
-
-        l = l[1..];
-        r = r[1..];
-
-    }
-
-
-    
-}
-
-Console.WriteLine(correct);
-
-
-//bool Unpack(string l, string r, out string left, out string right)
-//{
-//    if (l[0] == '[')
-//    {
-//        if (r[0] == '[')
-//        {
-//			left = l[1..];
-//			right = r[1..];
-
-//            return false;
-//        }
-//        else
-//        {
-//			left = l[1..];
-//			right = r.Insert(1, "]");
-//            return false;
-//        }
-//    }
-//    else if (r[0] == '[')
-//    {
-//        left = l.Insert(1, "]");
-//		right = r[1..];
-//		return false;
-//    }
-
-//    if (l[0] == ']' && r[0] == ']')
-//    {
-//		left = l[1..];
-//		right = r[1..];
-//		return false;
-//    }
-//	left = l;
-//	right = r;
-//	return true;
-//}
-
-(string left, string right) Unpack(string l, string r)
-{
-	if (l.Length > 2 && l[..2] == "[]")
-	{
-		return (l[1..], r[1..]);
-	}
-    if (l[0] == '[')
+    public static void Main(string[] args)
     {
-        if (r[0] == '[')
+        var input = File.ReadAllText(Utils.Input.GetInputFilePath(13)).Split("\n\n").Select(s => s.Split("\n")).Select(i => (i[0], i[1])).ToList();
+
+        //input = "[1,1,3,1,1]\n[1,1,5,1,1]\n\n[[1],[2,3,4]]\n[[1],4]\n\n[9]\n[[8,7,6]]\n\n[[4,4],4,4]\n[[4,4],4,4,4]\n\n[7,7,7,7]\n[7,7,7]\n\n[]\n[3]\n\n[[[]]]\n[[]]\n\n[1,[2,[3,[4,[5,6,7]]]],8,9]\n[1,[2,[3,[4,[5,6,0]]]],8,9]".Split("\n\n").Select(s => s.Split("\n")).Select(i => (i[0], i[1])).ToList();
+
+
+        List<int> correctPairs = new();
+        for (int i = 0; i < input.Count; i++)
         {
-            return Unpack(l[1..], r[1..]);
+            var curInp = input[i];
+
+            if (CompareLists(curInp.Item1, curInp.Item2) == 1)
+            {
+                correctPairs.Add(i + 1);
+            }
+
         }
-        else
-        {
-            return Unpack(l[1..], r.Insert(1, "]"));
-        }
+
+        Console.WriteLine(correctPairs.Sum());
+
     }
-    else if (r[0] == '[')
+    public static int CompareLists(string lInp, string rInp)
     {
-        return Unpack(l.Insert(1, "]"), r[1..]);
+        var curL = Unpack(lInp);
+        var curR = Unpack(rInp);
+
+        for (int l = 0; l < curL.Length; l++)
+        {
+            if (l >= curR.Length)
+            {
+                break;
+            }
+
+            var left = curL[l];
+            var right = curR[l];
+
+            if (IsList(left) || IsList(right))
+            {
+                var c = CompareLists(left, right);
+                if (c != 0)
+                {
+                    return c;
+                }
+                continue;
+            }
+
+            var comp = Compare(left, right);
+
+            if (comp < 0)
+            {
+                return 1;
+            }
+            if (comp > 0)
+            {
+                return -1;
+            }
+        }
+        if (curL.Length != curR.Length)
+        {
+            return curR.Length.CompareTo(curL.Length);
+        }
+        return 0;
     }
 
-    if (l[0] == ']' && r[0] == ']')
+
+    public static int Compare(string lInp, string rInp)
     {
-        return Unpack(l[1..], r[1..]);
+        var l = int.Parse(lInp);
+        var r = int.Parse(rInp);
+        return l.CompareTo(r);
     }
-    return (l, r);
+    public static bool IsList(string input)
+    {
+        return input.StartsWith("[");
+    }
+
+    public static string[] Unpack(string inp)
+    {
+        List<string> data = new();
+
+        if (!inp.StartsWith('['))
+        {
+            var comma = inp.IndexOf(',');
+            inp = inp.Insert(comma > 0 ? comma : inp.Length - 1, "]");
+            inp = inp.Insert(0, "[");
+        }
+
+        var endOfList = FindEndPositionForList(inp);
+        var toProcess = inp[1..endOfList];
+
+
+        var nesting = 0;
+        for (int i = 0, prev = 0; i < toProcess.Length; i++)
+        {
+            if (toProcess[i] == '[') nesting++;
+            if (toProcess[i] == ']') nesting--;
+            if (toProcess[i] == ',' && nesting == 0)
+            {
+                data.Add(toProcess[prev..i]);
+                prev = i + 1;
+            }
+            if (i == toProcess.Length - 1)
+            {
+                data.Add(toProcess[prev..]);
+            }
+        }
+
+        var rest = inp[(FindEndPositionForList(inp) + 1)..];
+        if (!string.IsNullOrEmpty(rest))
+        {
+            data.Add(rest);
+        }
+
+        return data.ToArray();
+    }
+
+
+    public static int FindEndPositionForList(string l)
+    {
+        if (l[0] != '[')
+        {
+            throw new ArgumentException();
+
+        }
+        int nesting = 0;
+
+        for (int i = 0; i < l.Length; i++)
+        {
+            if (l[i] == '[') nesting++;
+            if (l[i] == ']') nesting--;
+
+            if (nesting == 0)
+            {
+                return i;
+            }
+        }
+
+        throw new ArgumentException("Could not find list end");
+    }
 }
---
+++ Day13Tests/ProgramTests.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void CompareListsTest_SimpleListShouldReturn1()
+        {
+            int expected = 1;
+            var left = "[1,1,3,1,1]";
+            var right = "[1,1,5,1,1]";
+            var actual = Program.CompareLists(left, right);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void CompareListsTest_SimpleListLastCharShouldReturn1()
+        {
+            int expected = 1;
+            var left = "[1,1,5,1,1]";
+            var right = "[1,1,5,1,2]";
+            var actual = Program.CompareLists(left, right);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod()]
+        public void CompareListsTest_SimpleListShouldReturnNegative()
+        {
+            int expected = -1;
+            var left = "[1,1,5,1,1]";
+            var right = "[1,1,3,1,1]";
+            var actual = Program.CompareLists(left, right);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CompareListsTest_SimpleListShouldReturnNeutral()
+        {
+            int expected = 0;
+            var left = "[1,1,5,1,1]";
+            var right = "[1,1,5,1,1]";
+            var actual = Program.CompareLists(left, right);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CompareListsTest_Signal2ShouldReturnPositive()
+        {
+            int expected = 1;
+            var left = "[[1],[2,3,4]]";
+            var right = "[[1],4]";
+            var actual = Program.CompareLists(left, right);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CompareListsTest_Signal3ShouldReturnNegative()
+        {
+            int expected = -1;
+            var left = "[9]";
+            var right = "[[8,7,6]]";
+            var actual = Program.CompareLists(left, right);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CompareListsTest_Signal4ShouldReturnPositive()
+        {
+            int expected = 1;
+            var left = "[[4,4],4,4]";
+            var right = "[[4,4],4,4,4]";
+            var actual = Program.CompareLists(left, right);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CompareListsTest_Signal5ShouldReturnNegative()
+        {
+            int expected = -1;
+            var left = "[7,7,7,7]";
+            var right = "[7,7,7]";
+            var actual = Program.CompareLists(left, right);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CompareListsTest_Signal6ShouldReturnPositive()
+        {
+            int expected = 1;
+            var left = "[]";
+            var right = "[3]";
+            var actual = Program.CompareLists(left, right);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CompareListsTest_Signal7ShouldReturnNegative()
+        {
+            int expected = -1;
+            var left = "[[[]]]";
+            var right = "[[]]";
+            var actual = Program.CompareLists(left, right);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CompareListsTest_Signal4ShouldReturnNegative()
+        {
+            const int expected = -1;
+            var left = "[1,[2,[3,[4,[5,6,7]]]],8,9]";
+            var right = "[1,[2,[3,[4,[5,6,0]]]],8,9]";
+            var actual = Program.CompareLists(left, right);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+       
+    }
+}
